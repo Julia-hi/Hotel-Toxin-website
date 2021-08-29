48456bc70b89274e1263e82d5f3236ec766ca828
Julia <kuuerusa@gmail.com>
2021-08-29 13:03:22 +0200

    Gradient for radial border of buttons (div inside the button)

---
+++ js/lending_scripts.js
@@ -0,0 +1,13 @@
+
+
+
+
+function showCalendar() {
+    var x = document.getElementById("calendar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
